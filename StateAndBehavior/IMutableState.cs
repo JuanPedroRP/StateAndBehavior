b3c8c442dae0b32e5313c1af9dd9d5603e0c3ddf
Juan Pedro Ramos Ponce <juanpedrorp@yahoo.es>
2018-05-02 22:39:02 +0100

    Added IMutableState first sketch

---
+++ StateAndBehavior/IMutableState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateAndBehavior
+{
+    public interface IMutableState
+    {
+        void Set<T>(ItemKey itemKey, PropertyKey property, T value);
+
+        void UnSet(ItemKey itemKey, PropertyKey property);
+
+        void Splice();
+
+        void Invalidate(ItemKey itemKey);
+
+        void Dirty(ItemKey itemKey);
+
+        void Populate();
+    }
+}
